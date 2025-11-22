83ca007ac62b53a95d303bd48cd9e5edf7bacded
montoyay <montoyay@student.21-school.ru>
2025-11-22 18:38:33 +0300

    feat: work app

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+extern crate iron;
+#[macro_use]
+extern crate mime;
+
+extern crate router;
+use router::Router;
+
+extern crate urlencoded;
+use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    let mut router = Router::new();
+
+    router.get("/", get_form, "root");
+    router.post("/gcd", post_gcd, "gcd");
+
+    println!("Serving on http://localhost:3000");
+    Iron::new(router).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(
+        r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="post">
+            <input type="text" name="n"/>
+            <input type="text" name="n"/>
+            <button type="submit">Compute GCD</button>
+        </form>
+        "#,
+    );
+    Ok(response)
+}
+
+fn post_gcd(request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Err(e) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Error parsing form data: {:?}\n", e));
+            return Ok(response);
+        }
+        Ok(map) => map,
+    };
+
+    let unparsed_numbers = match form_data.get("n") {
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("form data has no 'n' param\n"));
+            return Ok(response);
+        }
+        Some(nums) => nums,
+    };
+
+    let mut numbers = Vec::new();
+    for unparsed in unparsed_numbers {
+        match u64::from_str(&unparsed) {
+            Err(..) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!(
+                    "value for 'n' param not a number: {:?}\n",
+                    unparsed
+                ));
+                return Ok(response);
+            }
+            Ok(n) => {
+                numbers.push(n);
+            }
+        }
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(format!(
+        "the greatest common divisor of the numbers {:?} is <b>{}</b>\n",
+        numbers, d
+    ));
+    Ok(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            swap_nums(&mut m, &mut n);
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+    assert_eq!(gcd(2, 4), 2);
+}
+
+fn swap_nums(a: &mut u64, b: &mut u64) {
+    let t = *a;
+    *a = *b;
+    *b = t;
+}
